c837b33e8d6a9df8b5ccd39ada156458bb2a8036
itsaxend <axendround2@gmail.com>
2025-03-05 01:57:21 +0700

    update as i forgot to force it hehe

--- auto-symlink.sh
+++ auto-symlink.sh
@@ -14,4 +14,4 @@ ln -s ~/dotfiles/Wallpapers/ ~/Pictures/
 
 mkdir ~/.local/share/icons/
 cp -r ~/dotfiles/Bibata-Modern-Ice/ ~/.local/share/icons/
-sudo cp -r ~/dotfiles/Bibata-Modern-Ice/ /usr/share/icons/ 
+sudo cp -rf ~/dotfiles/Bibata-Modern-Ice/ /usr/share/icons/ 
