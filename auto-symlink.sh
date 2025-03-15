db80125f04be08abc6ca7c4897bd809e270e855c
itsaxend <axendround2@gmail.com>
2025-03-15 16:50:59 +0700

    updates

--- auto-symlink.sh
+++ auto-symlink.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+read -rp "Wanna symlink 'hypr' or copy it? (symlink/copy): " hypr
+if [[ $hypr == "symlink" || $hypr == "sym" ]]; then
+    ln -s ~/dotfiles/config/hypr/ ~/.config/
+else
+    cp -r ~/dotfiles/config/hypr/ ~/.config/
+fi
+
 ln -s ~/dotfiles/config/cava/ ~/.config/
-ln -s ~/dotfiles/config/hypr/ ~/.config/
 ln -s ~/dotfiles/config/nvim/ ~/.config/
 ln -s ~/dotfiles/config/rofi/ ~/.config/
 ln -s ~/dotfiles/config/kitty/ ~/.config/
