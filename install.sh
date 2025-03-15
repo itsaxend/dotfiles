fa04be5c48446b4e0f1f5f73f5589d3e1d9d8a22
itsaxend <axendround2@gmail.com>
2025-03-16 01:09:47 +0700

    updates

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-source /scripts/packages.sh
-source /scripts/symlink.sh
-source /scripts/yay-zsh.sh
 
 echo ""
 sleep 0.5
@@ -28,55 +25,8 @@ else
     exit 1 
 fi
 
-read -rp "Ready to install packages? (y/n): " install 
-if [[ $install == "y" || $install == "yes" ]]; then
-    read -rp "AMD or Intel CPU? " cpu
-    if [[ $cpu == "amd" || $cpu == "AMD" ]]; then
-        read -rp "Wanna remove specified packages? (y/n): " rsp
-        if [[ $rsp == "y" || $rsp == "yes" ]]; then
-            amd
-            install_pacman_packages
-            install_yay_packages
-            uninstall_specified_packages
-            echo "Removed specified packages."
-            echo "Done!"
-        elif [[ $rsp == "n" || $rsp == "no" ]]; then
-            amd
-            install_pacman_packages
-            install_yay_packages
-            echo "Skipped removing specified packages." 
-            echo "Done!"
-        else
-            echo "Invalid option, choose 'y' or 'n'"
-            exit 1
-        fi
-    elif [[ $cpu == "intel" || $cpu == "Intel" ]]; then
-        read -rp "Wanna remove specified packages? (y/n): " rsp
-        if [[ $rsp == "y" || $rsp == "yes" ]]; then
-            intel
-            install_pacman_packages
-            install_yay_packages
-            uninstall_specified_packages
-            echo "Removed specified packages."
-            echo "Done!"
-        elif [[ $rsp == "n" || $rsp == "no" ]]; then
-            intel
-            install_pacman_packages
-            install_yay_packages
-            echo "Skipped removing specified packages." 
-            echo "Done!"
-        else
-            echo "Invalid option, choose 'y' or 'n'"
-            exit 1
-        fi
-    else
-        echo "Invalid input. type 'amd' or 'intel'"
-        exit 1 
-    fi
-else
-    echo "Exiting."
-    exit 1
-fi
+echo "installing packages ..."
+bash /scripts/packages.sh
 
 echo "Symlink ..."
 bash /scripts/symlink.sh
