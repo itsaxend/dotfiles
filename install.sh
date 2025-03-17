f3c5a8f29ee2526afd6944034b2ad703d6e1e25e
itsaxend <axendround2@gmail.com>
2025-03-17 16:58:22 +0700

    updates

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 echo ""
 sleep 0.5
-echo "$(tput setaf 196)HEAD UP !!! This script is just for my personal use.$(tput sgr0)"
+echo "$(tput setaf 196)HEAD UP !!! I dont know how you managed to find this dotfiles, but its ONLY FOR MY PERSONAL USE.$(tput sgr0)"
 echo "$(tput setaf 184)You should cancel this by hitting CTRL-C and read the script before starting.$(tput sgr0)"
 echo ""
 sleep 0.5
 
-read -rp "Start install yay & zsh? (y/n): " begin
+read -rp "Start install? (y/n): " begin
 if [[ $begin == "y" || $begin == "yes" ]]; then
-    sudo pacman -S --needed --noconfirm git base-devel
+    echo "Updating system ..."
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S --needed --noconfirm git base-devel linux-zen-headers
 
     if ! command -v yay &> /dev/null; then
         git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
