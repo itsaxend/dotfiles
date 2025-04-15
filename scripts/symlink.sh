bfd3fa398913ca0ff832ed145d7108ed91154cdf
itsaxend <axendround2@gmail.com>
2025-04-15 12:54:16 +0700

    updates

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -9,46 +9,46 @@ RESET="$(tput sgr0)"
 
 read -rp "${SKY_BLUE}NVIDIA card? (y/n): ${RESET}" nvidia
 if [[ $nvidia == "y" || $nvidia == "yes" ]]; then
-    sed -i 's/^# \(source = $configs\/nvidia.conf\)/\1/' ~/dotfiles/config/hypr/hyprland.conf
+    sed -i 's/^# \(source = $configs\/nvidia.conf\)/\1/' $HOME/dotfiles/config/hypr/hyprland.conf
 fi
 
 read -rp "${SKY_BLUE}Wanna symlink 'hypr' or copy it? (symlink/copy): ${RESET}" hypr
 if [[ $hypr == "symlink" || $hypr == "s" ]]; then
-    ln -sf ~/dotfiles/config/hypr/ ~/.config/
+    ln -sf $HOME/dotfiles/config/hypr/ $HOME/.config/
 elif [[ $hypr == "copy" || $hypr == "c" ]]; then
-    cp -rf ~/dotfiles/config/hypr/ ~/.config/
+    cp -rf $HOME/dotfiles/config/hypr/ $HOME/.config/
 else
   echo "${WARNING}Invalid option, choose 'symlink' or 'copy'${RESET}"
   exit 1
 fi
 
-ln -sf ~/dotfiles/config/cava/ ~/.config/
-ln -sf ~/dotfiles/config/nvim/ ~/.config/
-ln -sf ~/dotfiles/config/rofi/ ~/.config/
-ln -sf ~/dotfiles/config/kitty/ ~/.config/
-ln -sf ~/dotfiles/config/swaync/ ~/.config/
-ln -sf ~/dotfiles/config/waybar/ ~/.config/
-ln -sf ~/dotfiles/config/wallust/ ~/.config/
-ln -sf ~/dotfiles/config/fastfetch/ ~/.config/
+ln -sf $HOME/dotfiles/config/cava/ $HOME/.config/
+ln -sf $HOME/dotfiles/config/nvim/ $HOME/.config/
+ln -sf $HOME/dotfiles/config/rofi/ $HOME/.config/
+ln -sf $HOME/dotfiles/config/kitty/ $HOME/.config/
+ln -sf $HOME/dotfiles/config/swaync/ $HOME/.config/
+ln -sf $HOME/dotfiles/config/waybar/ $HOME/.config/
+ln -sf $HOME/dotfiles/config/wallust/ $HOME/.config/
+ln -sf $HOME/dotfiles/config/fastfetch/ $HOME/.config/
 
-if [ ! -d "~/Pictures" ]; then
-    mkdir -p ~/Pictures
+if [ ! -d "$HOME/Pictures" ]; then
+    mkdir -p $HOME/Pictures
 fi
-ln -sf ~/dotfiles/Wallpapers/ ~/Pictures/
+ln -sf $HOME/dotfiles/Wallpapers/ $HOME/Pictures/
 
-if [ ! -d "~/.local/share/icons" ]; then
-    mkdir -p ~/.local/share/icons/
+if [ ! -d "$HOME/.local/share/icons" ]; then
+    mkdir -p $HOME/.local/share/icons/
 fi
-cp -rf ~/dotfiles/Bibata-Modern-Ice/ ~/.local/share/icons/
-sudo cp -rf ~/dotfiles/Bibata-Modern-Ice/ /usr/share/icons/ 
+cp -rf $HOME/dotfiles/Bibata-Modern-Ice/ $HOME/.local/share/icons/
+sudo cp -rf $HOME/dotfiles/Bibata-Modern-Ice/ /usr/share/icons/ 
 
-if [ -d "~/.oh-my-zsh" ]; then
-    rm -rf ~/.oh-my-zsh
-fi
-
-if [ -f "~/.zshrc" ]; then
-    rm -rf ~/.zshrc
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    rm -rf $HOME/.oh-my-zsh/themes
 fi
 
-ln -sf ~/dotfiles/.oh-my-zsh ~/
-ln -sf ~/dotfiles/.zshrc ~/
+if [ -f "$HOME/.zshrc" ]; then
+    rm -rf $HOME/.zshrc
+fi
+
+ln -sf $HOME/dotfiles/ohmyzsh/themes $HOME/.oh-my-zsh/
+ln -sf $HOME/dotfiles/.zshrc $HOME/
