c04b2b9a2de1e106f2247b8c2519c439b43eccce
itsaxend <axendround2@gmail.com>
2025-05-12 15:38:31 +0700

    yeh

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.opt.clipboard = "unnamedplus"
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
+vim.opt.cursorline = true
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
