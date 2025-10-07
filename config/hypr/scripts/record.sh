e6916ef0377346a6794c4f41ee689d63f69eadb3
itsaxend <axendround2@gmail.com>
2025-10-07 16:30:46 +0700

    yeh

--- config/hypr/scripts/record.sh
+++ config/hypr/scripts/record.sh
@@ -6,7 +6,7 @@ if pgrep wf-recorder >/dev/null; then
     pkill -SIGTERM wf-recorder
     notify-send -t 650 "Screen Recording" "Stopped recording."
 else
-    wf-recorder --audio=alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo.monitor -r 60 -f "$OUTPUT_FILE" &
+    wf-recorder --audio=alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo.monitor --bitrate 50000 -c libx264rgb -r 144 -f "$OUTPUT_FILE" &
     notify-send -t 650 "Screen Recording" "Recording started"
 fi
 
